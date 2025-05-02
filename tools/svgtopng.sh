d693cfba16492821409e6500f352e006ac74aa25
snona-tech <eulersconst0.577@gmail.com>
2025-05-02 15:33:47 +0900

    特殊な文字を含むファイル名の画像が変換できない問題の対処

--- tools/svgtopng.sh
+++ tools/svgtopng.sh
@@ -9,11 +9,32 @@ sed -i 's/<?xml version="1.0" encoding="utf-16"?>/<?xml version="1.0" encoding="
 
 sed -i '/metadata/,/metadata/d' tools/landscape/hosted_logos/Seldon_idWf-VLClV_10.svg
 
+special_filename=(
+  'banco-de-cre'$'\314\201''dito-bcp.svg'
+  'banco-de-cr'$'\303\251''dito-bcp.svg'
+  ''$'\303\246''nix.svg'
+)
+
+tmp_filename=tmp
+count=1
+
+for filename in "${special_filename[@]}"; do
+  mv tools/landscape/hosted_logos/${filename} tools/landscape/hosted_logos/${tmp_filename}${count}
+  ((count++))
+done
+
 docker run --rm -it \
   -v ${PWD}/tools/landscape/hosted_logos:/hosted_logos \
   -v ${PWD}/tools/png:/png \
   vulhub/librsvg:2.50.7 \
   bash -c 'apt-get update;apt-get install -y pngquant;ls hosted_logos/ | xargs -I{} -P 3 -n 1 rsvg-convert hosted_logos/{} -o png/{}.png;pngquant --ext .png --force --speed 1 png/*.png'
+
+count=1
+
+for filename in "${special_filename[@]}"; do
+  mv tools/png/${tmp_filename}${count}.png tools/png/${filename}.png
+  ((count++))
+done
 
 echo "SVG 数：$(ls tools/landscape/hosted_logos | wc -l)"
 echo "PNG 数：$(ls tools/png | wc -l)"
